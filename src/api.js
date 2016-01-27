38f2fb6f5d9024876f9a9dbca8517f9d1f94f9e4
BruceWeng <weng8916@gmail.com>
2016-01-27 01:58:12 -0800

    Creating the Chirp Input Component (Nested content in Home Component)

--- src/api.js
+++ src/api.js
@@ -1,8 +1,16 @@
 var actions = require('./actions');
+var dispatcher = require('./dispatcher');
+var constants = require('./constants');
 
 var API = module.exports = {
     fetchChirps: function () {
         get('/api/chirps').then(actions.gotChirps.bind(actions));
+    },
+    saveChirp: function (text) {
+        text = text.trim();
+        if(text === '') return;
+
+        post('/api/chirps', { text: text}).then(actions.chirped.bind(actions));
     }
 };
 
@@ -14,3 +22,26 @@ function get (url) {
         return response.json();
     });
 }
+
+function post (url, body) {
+    return fetch(url, {
+        method:'POST',
+        credentials: 'include',
+        body: JSON.stringify(body || {}),
+        headers: {
+            'Content-Type' : 'application/json',
+            'Accept' : 'application/json'
+        }
+    }).then(function (response) {
+        return response.json();
+    });
+}
+
+dispatcher.register(function (action) {
+    switch (action.actionType) {
+      case constants.CHIRP:
+          API.saveChirp(action.data);
+          break;
+
+    }
+});
