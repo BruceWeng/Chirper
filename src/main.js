b96ffaad25c3c3a50bd5e41d7bcea4918279e4b3
BruceWeng <weng8916@gmail.com>
2016-01-27 01:04:21 -0800

    Writing the App Component (React start here)

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
+var React = require('react');
+var ReactRouter = require('react-router'); // allow us to write route in jsx format
+
+var Route = ReactRouter.Route;
+
 var API = require('./api');
 var ChirpStore = require('./stores/chirps');
 
+var routes = (<Route handler={require('./components/App')}> </Route>);
+
 API.fetchChirps();
+
+ReactRouter.run(routes, ReactRouter.HistoryLocation, function (Root) {
+    React.render(<Root />, document.getElementById('app')); // render(object, location)
+});
