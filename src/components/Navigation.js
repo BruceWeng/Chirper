28fd44ca843bd6636f435337109812217e4a16ad
BruceWeng <weng8916@gmail.com>
2016-06-08 19:48:12 -0700

    Change Timeline to news Feed

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -10,7 +10,7 @@ var Navigation = module.exports = React.createClass({
     },
     render: function () {
         return <ul>
-            <li> <Link to='home'> Timeline </Link> </li>
+            <li> <Link to='home'> News Feed </Link> </li>
             <li> <Link to='users'> Users </Link> </li>
             <li> <a href='/logout'> Logout </a> ({this.state.username}) </li>
         </ul>;
